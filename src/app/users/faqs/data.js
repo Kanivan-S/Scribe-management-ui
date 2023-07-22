65f7c0303027f5e1038bc71f82173cf848a9f6c0
Ashish-Verma-MNNIT <ashishkv.2016@gmail.com>
2023-07-22 22:09:19 +0530

    faq done

---
+++ src/app/users/faqs/data.js
@@ -0,0 +1,54 @@
+const data = [
+  {
+    question: "How do I register as a volunteer?",
+    answer:
+      "To register as a volunteer, click on the 'Register' button on the homepage and fill in the required details in the registration form. You can login with the same credentials later on.",
+  },
+  {
+    question: "What are the available volunteering opportunities?",
+    answer:
+      "You can view all available volunteering opportunities by logging into your account and visiting the Upcoming events page. You can volunteer for a particular event available in that page.",
+  },
+  {
+    question: "How do I sign up for a specific volunteering opportunity?",
+    answer:
+      "To sign up for a volunteering opportunity, click on the event or project you're interested in, and then click the 'Sign Up' button. You may need to provide additional information or agree to certain terms and conditions.",
+  },
+  {
+    question: "Can I track my previous volunteering  activities?",
+    answer:
+      "Yes, you can track your volunteering activities in your profile. The system automatically records the hours you've volunteered for each event.",
+  },
+  {
+    question: "What happens after I sign up for an event?",
+    answer:
+      "After signing up, you will receive a confirmation email with details about the event, including the date, time, and location. Make sure to mark your calendar and attend the event on the specified date.",
+  },
+  {
+    question: "Can I cancel my participation in an event?",
+    answer: "Yes, you can cancel your participation, but avoid doing that.",
+  },
+  {
+    question: "What are the benefits of volunteering?",
+    answer:
+      "Volunteering offers numerous benefits, including skill development, networking opportunities, and the satisfaction of making a positive impact on the community.",
+  },
+  {
+    question: "Do I need any specific skills or qualifications to volunteer?",
+    answer:
+      "While some volunteering opportunities may require specific skills or qualifications, many activities are open to individuals with a passion for helping others.",
+  },
+  {
+    question: "What do i have to do after volunteering?",
+    answer:
+      "After confirmation of your volunteering, you will need to be available on the date of volunteering and have to help the mapped student in completion of his/her examination.",
+  },
+  {
+    question:
+      "Is there any way I can keep track of my upcoming event/activity?",
+    answer:
+      "Don't worry, we have got you covered! We have a calender implementation that will clearly show you the events you have signed up for and about your upcoming acitivities!",
+  },
+];
+
+export default data;
