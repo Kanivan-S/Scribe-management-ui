b01e857a0c53357350abeb1fa003f8da275da9c1
AakashArvind <aakashsarath@gmail.com>
2023-07-23 13:48:06 +0530

    Added chatbot

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    dialogflow: {
+      projectId: 'faq-rvdy',
+      languageCode: 'en-US'
+    },
+    // Add other environment variables as needed
+  };
+  
